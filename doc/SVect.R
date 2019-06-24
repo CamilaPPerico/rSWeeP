12d23dae7bb38aae8f0f7378b566be7a89cc5e92
DanrleyRF <51912065+DanrleyRF@users.noreply.github.com>
2019-06-24 09:22:55 -0300

    up

---
+++ doc/SVect.R
@@ -0,0 +1,10 @@
+## --------------------------------------------------------------------------
+library(SVect)
+baseMatrix <- orthbase(160000,10)
+data(datastring)
+cat(datastring, file = "exdna.fas", sep = "\n")
+return <- SWeeP("exdna.fas",baseMatrix)
+distancia <- dist(return, method = "euclidean")
+tree <- hclust(distancia, method="ward.D")
+plot(tree, hang = -1, cex = 1)
+
